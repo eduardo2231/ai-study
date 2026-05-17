89b21a3639f18fbd692eeae6f15f2b0164455415
Eduardo Bonometti <bonomettieduardo2@gmail.com>
2026-05-17 15:43:40 -0300

    Update audio.py

--- services/audio.py
+++ services/audio.py
@@ -13,30 +13,29 @@ def baixar_midia(url: str, pasta_saida="downloads"):
             arquivo_final = d["filename"]
 
     opcoes = {
-    "format": "bestaudio/best",
-    "outtmpl": f"{pasta_saida}/%(title)s.%(ext)s",
-    "restrictfilenames": True,
-    "noplaylist": True,
-    "quiet": True,
-    "no_warnings": True,
-    "retries": 5,
-    "fragment_retries": 5,
-    "http_headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/122 Safari/537.36"
-    },
-    "extractor_args": {
-        "youtube": {
-            "player_client": ["android"]
-        }
-    },
+        "format": "bestaudio/best",
+        "outtmpl": f"{pasta_saida}/%(title)s.%(ext)s",
+        "restrictfilenames": True,
+        "noplaylist": True,
+        "quiet": False,
+        "no_warnings": True,
+        "retries": 3,
+        "fragment_retries": 3,
+
+        "progress_hooks": [hook],
+
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }],
     }
 
     try:
         with yt_dlp.YoutubeDL(opcoes) as ydl:
 
             info = ydl.extract_info(url, download=True)
-            
-            arquivo_final = ydl.prepare_filename(info)
+
             arquivo_final = os.path.splitext(arquivo_final)[0] + ".mp3"
 
             print("\n========== INFO ==========")
